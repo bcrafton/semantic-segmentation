8c1d4a6c3d80edc2418a73c8e73cba99701822ba
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-13 11:31:12 -0400

    issue was with mx instead of numpy

--- code/segnet.py
+++ code/segnet.py
@@ -7,8 +7,11 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 import numpy as np
+import mxnet as mx
+
 import tensorflow as tf
 import keras
+
 from gluoncv.data import CitySegmentation
 
 from lib.SegNet import SegNet
@@ -21,13 +24,13 @@ from lib.SegNet import SegNet
 val_dataset = CitySegmentation(split='val')
 val_examples = len(val_dataset)
 
-batch_size = 5
+batch_size = 1
 epochs = 10
 
 ####################################
 
-image         = tf.placeholder(tf.float32, [5, 480, 480, 3])
-label         = tf.placeholder(tf.int64, [5, 480, 480])
+image         = tf.placeholder(tf.float32, [batch_size, 480, 480, 3])
+label         = tf.placeholder(tf.int64, [batch_size, 480, 480])
 label_one_hot = tf.one_hot(label, depth=30, axis=-1)
 
 model   = SegNet(batch_size=batch_size, init='glorot_uniform')
@@ -36,11 +39,8 @@ predict = tf.argmax(tf.nn.softmax(out), axis=3)
 
 ####################################
 
-correct = tf.equal(predict, label_one_hot)
+correct = tf.equal(predict, label)
 sum_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
-
-loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=out)
-train = tf.train.AdamOptimizer(learning_rate=1e-2, epsilon=1.).minimize(loss)
 
 ####################################
 
@@ -51,15 +51,10 @@ for ii in range(epochs):
 
     total_correct = 0 
     for jj in range(0, 500, batch_size):
-
-        xs = []; ys = []
-        for kk in range(batch_size):
-            x, y = val_dataset[jj + kk]
-            xs.append(x)
-            ys.append(y)
-        xs = np.stack(xs, axis=0)
-        ys = np.stack(ys, axis=0)
-
+        print (jj)
+        x, y = val_dataset[jj]
+        xs = x.asnumpy().reshape([1, 480, 480, 3])
+        ys = y.asnumpy().reshape([1, 480, 480])
         [_sum_correct] = sess.run([sum_correct], feed_dict={image: xs, label: ys})
         total_correct += _sum_correct
 
