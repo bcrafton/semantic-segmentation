2078f01d25bd970ea0d8a07707cb62058b3afc7a
Brian Crafton <crafton.b@husky.neu.edu>
2019-11-15 12:39:59 -0500

    this was changed

--- code/cityscapes.py
+++ code/cityscapes.py
@@ -1,35 +1,24 @@
 
-# following this tutorial to check out city scapes.
-# https://gluon-cv.mxnet.io/build/examples_datasets/cityscapes.html
-
 from gluoncv.data import CitySegmentation
+import matplotlib.pyplot as plt
+import numpy as np
+
+#########################
+
 train_dataset = CitySegmentation(split='train')
 val_dataset = CitySegmentation(split='val')
+
 print('Training images:', len(train_dataset))
 print('Validation images:', len(val_dataset))
 
 #########################
 
-import numpy as np
-img, mask = val_dataset[0]
-# get pallete for the mask
-from gluoncv.utils.viz import get_color_pallete
-mask = get_color_pallete(mask.asnumpy(), dataset='citys')
-mask.save('mask.png')
+for ii in range(100):
+    print (ii)
+    img, mask = val_dataset[ii]
+    img = img.asnumpy()
+    img = img / np.max(img)
+    plt.imsave('%d.jpg' % (ii), img)
 
 #########################
 
-from matplotlib import pyplot as plt
-import matplotlib.image as mpimg
-# subplot 1 for img
-fig = plt.figure()
-fig.add_subplot(1,2,1)
-plt.imshow(img.asnumpy().astype('uint8'))
-# subplot 2 for the mask
-mmask = mpimg.imread('mask.png')
-fig.add_subplot(1,2,2)
-plt.imshow(mmask)
-# display
-plt.show()
-
-#########################
